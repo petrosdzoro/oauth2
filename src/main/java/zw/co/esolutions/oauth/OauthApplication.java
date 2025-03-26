5e14105aa4f11d4468ae05d5c325913ce603c4fc
petrosdzoro@gmail.com <petrosdzoro@gmail.com>
2025-03-26 11:45:06 +0200

    Initial Commit

---
+++ src/main/java/zw/co/esolutions/oauth/OauthApplication.java
@@ -0,0 +1,15 @@
+package zw.co.esolutions.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/esolutions/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.esolutions.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
